1b960aeb17452030ee3b3d15cdff0006d551304a
Piotr Gawron <norwagov43@gmail.com>
2022-09-29 20:24:46 +0200

    1st librus attempt

--- app/Http/Controllers/LibrusController.php
+++ app/Http/Controllers/LibrusController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use DOMDocument;
+use DOMXPath;
+use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Contracts\Support\Renderable;
+require '../vendor/autoload.php';
 
 class LibrusController extends Controller
 {
@@ -14,5 +18,19 @@ class LibrusController extends Controller
         return view('gradebook.index');
     }
 
-
+    /**
+     * @throws GuzzleException
+     */
+    public function getData($login, $pass) {
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('POST', 'https://api.librus.pl/OAuth/Authorization?client_id=46', [
+            'form_params' => [
+                'action' => 'login',
+                'username' => $login,
+                'password' => $pass
+            ]
+        ]);
+        echo $response->getBody();
+    }
 }
+
--- resources/views/gradebook/index.blade.php
+++ resources/views/gradebook/index.blade.php
@@ -1,9 +1,51 @@
 @extends('layouts.app')
 
+<?php namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth; ?>
+@section('javascript2')
+
+@endsection
+
 @section('content')
-
-    <div id="app">
-
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div style="display: flex; justify-content: space-between; align-items: center;">
+                            <span id="card_title">
+                                {{ __('Gradebook') }}
+                            </span>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-striped table-hover">
+                                <thead class="thead">
+                                <tr>
+                                    <th>Subject</th>
+                                    <th>Grade</th>
+                                    <th>Description</th>
+                                    <th>Teacher</th>
+                                </tr>
+                                    <?php
+                                LibrusController::getData(Auth::user()->librus_id, Auth::user()->librus_password);
+                                ?>
+                                    <!--oreach($grades as $grade)
+                                    <tr>
+                                        <td>$grade->subject}}</td>
+                                        <td>$grade->grade}}</td>
+                                        <td>{$grade->description}}</td>
+                                        <td>{$grade->teacher}}</td>
+                                    </tr>
+                                endforeach-->
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-
 @endsection
