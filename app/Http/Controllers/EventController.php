9170b41fa10b4cf9b4980e0b3f2eed181ac7b778
SzymonEls <malpkamalpka58@gmail.com>
2022-10-28 22:09:47 +0200

    comments in EventController

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,7 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+class Time {
+    //attributes
+    public $value = 1;
+    public $arrange = array();
+}
+
 class EventController extends Controller
 {
-    //
+    //segregateTime
+    //rearrangeTime
+
+    public function segregateTime(): void {
+        //Pobiera użytkownika
+        //Pętla po dniach od dzisiaj do x => x = ostatni_event.dzień + 7
+            //Pobiera godziny lekcji przed i po.
+            //Pobiera start i koniec dnia (NOWE USTAWIENIE) [GODZINY PRACY]
+            //Pobieranie eventów z tego dnia
+            //Liczy optymalny czas pracy tego dnia i maksymalny pracy tego dnia
+                //OPTIMAL: MAX / 2
+                //MAXIMAL: 1440 => 1440 = min_dnia => 1440 - [ CZAS PRACY + PRZERWY(set_by_user) ]
+
+    }
 }
